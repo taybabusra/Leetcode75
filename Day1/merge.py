d6397118c8c56a3464325e9411da0108c100dbc3
TAYBA BUSRA <taybabusra@gmail.com>
2024-02-01 17:59:26 +0600

    Update merge.py

--- Day1/merge.py
+++ Day1/merge.py
@@ -1,1 +1,13 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        i,j = 0,0
+        res =[]
 
+        while i< len(word1) and  j<len(word2) :
+            res.append(word1[i])
+            res.append(word2[j])
+            i+=1
+            j+=1
+        res.append(word1[i:])  # if word1 length is greater than word2
+        res.append(word2[j:])  # if word2 length is greater than word1
+        return "".join(res)
