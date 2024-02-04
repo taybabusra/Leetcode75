3f9cfc3635086f2fce73e460e0795bcbe86d8dd7
TAYBA BUSRA <taybabusra@gmail.com>
2024-02-04 23:13:37 +0600

    Update place_flower.py

--- Day4/place_flower.py
+++ Day4/place_flower.py
@@ -1,14 +1,2 @@
-class Solution:
-    def canPlaceFlowers(self, flowerbed, n):
-        count = 0
-        i = 0
-        while i < len(flowerbed):
-            if flowerbed[i] == 0:
-                if i == 0 or flowerbed[i - 1] == 0:
-                    if i == len(flowerbed) - 1 or flowerbed[i + 1] == 0:
-                        flowerbed[i] = 1
-                        count += 1
-                        i += 1
-            i += 1
-        return count >= n
+
 
