643d6e473ca956f402e09a84b3899daec8ebd2c7
TAYBA BUSRA <taybabusra@gmail.com>
2024-02-07 20:52:26 +0600

    Update reverse.py

--- Day7/reverse.py
+++ Day7/reverse.py
@@ -1,4 +1,7 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
+        # Split the input string `s` into words using the split() method
+        # Reverse the order of the words using the reversed() function
+        # Join the reversed words into a single string separated by a space using the join() method
         return " ".join(reversed(s.split()))
-        
+
