f2f616263b8243fdab8df3720e447d10493fb586
TAYBA BUSRA <taybabusra@gmail.com>
2024-06-05 13:27:50 +0600

    Add files via upload

---
+++ search_insert.py
@@ -0,0 +1,18 @@
+#search insert position
+#problem_35
+time complexity:O(log n)
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left =0
+        right = len(nums)-1
+
+        while left<=right:
+            mid =(left+right)//2
+            if nums[mid]==target:
+                return mid
+            elif target>nums[mid]:
+                left = mid+1
+            else:
+                right = mid -1
+        return left
---
+++ srqt_x.py
@@ -0,0 +1,8 @@
+#problem:69
+
+# sqrt of a number return in round figure
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        import math
+        return int(math.sqrt(x))
